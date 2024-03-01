686ceb66507724528889aadf06ff0959b9ea9820
cyril <cyrildaniels21+git@gmail.com>
2024-03-01 03:20:26 +0100

    fix bug

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Print the body of a http 200 OK response.
-curl -s -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
+curl -sL -w "%{http_code}" "$1" | grep -q 200 && curl -sL "$1"
